4268195db719125eb83a738b89d0ff28fd2116c4
Francisco Eduardo <franciscoeduardo@MacBook-Air-de-Francisco.local>
2023-04-28 08:20:29 -0300

    Register screen style updated

--- src/screens/Register/Register.style.ts
+++ src/screens/Register/Register.style.ts
@@ -3,13 +3,17 @@ import styled from "styled-components";
 export const RegisterContainer = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: stretch;
+    align-items: center;
     width: 30rem;
 `
 
 export const Title = styled.div`
   color: white;
   font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
 `
 
 export const RegisterForm = styled.form`
@@ -30,13 +34,16 @@ export const LabelRegister = styled.label`
 `
 
 export const InputRegister = styled.input`
+  padding: 1rem;
+  border: 1px solid #333333;
+  border-radius: 0.5rem;
+  background: #555555;
+  outline: none;
+
+  color: white;
+  font-size: 1rem;
+  font-weight: 400;
   width: 100%;
-  height: 36px;
-  border: none;
-  border-radius: 8px;
-  padding-left: 15px;
-  font-size: 16px;
-  color: #484848;
 `
 
 export const ButtonRegister = styled.button`
@@ -46,7 +53,12 @@ export const ButtonRegister = styled.button`
   margin-top: 20px;
   height: 48px;
   font-size: 24px;
-  background-color: gold;
-  color: #242424;
+  background-color: #2D59B7;
+  color: white;
   cursor: pointer;
+  transition: all 0.5s;
+
+  &:hover{
+      filter: brightness(120%);
+  }
 `
